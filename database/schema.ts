f821ba4a9251790ecbcc64bfe4d57fe9ff96fdba
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2025-07-26 00:16:06 +0700

    feat: 🎸 upgrade boilerplate version at 2025-07-26

--- database/schema.ts
+++ database/schema.ts
@@ -1,1 +1,5 @@
-export * from "./schema/showcase";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const demo = sqliteTable("demo", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+});
